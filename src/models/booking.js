6adf10aa4c8ca15522242252d08373595a417691
Vinod Kumar <vinodrao835@gmail.com>
2025-01-27 11:45:13 +0530

    FEAT: Added the Booking Model with enum from Flight Service. ``` npx sequelize db:migrate ```

---
+++ src/models/booking.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+
+const { Enum } = require("../utils/common");
+
+const { BOOKED, CANCELLED, INITIATED, PENDING } = Enum.BOOKING_STATUS;
+
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+
+    static associate(models) {
+
+    }
+
+  }
+  Booking.init({
+    flightId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM,
+      values: [BOOKED, CANCELLED, INITIATED, PENDING],
+      defaultValue: INITIATED,
+      allowNull: false
+    },
+    noOfSeats: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
+      allowNull: false
+    },
+    totalCost: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
---
+++ src/utils/common/enums.js
@@ -0,0 +1,38 @@
+
+
+
+const SEAT_TYPE = {
+    BUSINESS: "business",
+    ECONOMY: "economy",
+    PREMIUM_ECONOMY: "premium-economy",
+    FIRST_CLASS: "first-class"
+}
+
+const SEAT_COLUMNS = {
+    A: "a",
+    B: "b",
+    C: "c",
+    D: "d",
+    E: "e",
+    F: "f",
+    G: "g"
+}
+
+const SEAT_STATUS = {
+    BOOKED: "booked",
+    AVAILABLE: "available"
+}
+
+const BOOKING_STATUS = {
+    BOOKED: "booked",
+    CANCELLED: "cancelled",
+    INITIATED: "initiated",
+    PENDING: "pending"
+};
+
+module.exports = {
+    SEAT_TYPE,
+    SEAT_COLUMNS,
+    SEAT_STATUS,
+    BOOKING_STATUS
+}
